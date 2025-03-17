c6096b1b1e7352c0417f71a0e2ac18ad15266151
felipeangeli <felipe.angeli@gmail.com>
2025-03-16 21:22:27 -0300

    add type em models e cores nos cards

--- lib/views/widget/pokemon_card.dart
+++ lib/views/widget/pokemon_card.dart
@@ -1,4 +1,5 @@
 import 'package:desafio_poke_api/models/pokemon_models.dart';
+import 'package:desafio_poke_api/utils/type_colors.dart';
 import 'package:flutter/material.dart';
 
 class PokemonCard extends StatelessWidget {
@@ -20,6 +21,14 @@ class PokemonCard extends StatelessWidget {
     return '${pokemon.name[0].toUpperCase()}${pokemon.name.substring(1)}';
   }
 
+  Color get _cardColor {
+    if (pokemon.types.isNotEmpty) {
+      final primaryType = pokemon.types.first.toLowerCase();
+      return TypeColors.colors[primaryType] ?? Colors.white;
+    }
+    return Colors.white;
+  }
+
   Widget _buildDetails() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -27,6 +36,7 @@ class PokemonCard extends StatelessWidget {
         Text('Altura: ${pokemon.height ?? 'Altura'}'),
         Text('Peso: ${pokemon.weight ?? 'Peso'}'),
         Text('Experiência: ${pokemon.baseExperience ?? 'Experiência'}'),
+        Text('Categoria: ${pokemon.types.isNotEmpty ? pokemon.types.join(', ') : 'Categoria'}'),
       ],
     );
   }
@@ -34,6 +44,7 @@ class PokemonCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: _cardColor.withOpacity(0.3),
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       elevation: 4,
       child: ListTile(
